11dae098f31dbe0a58154cda020073bc1ffea72a
Ediellyf <ediellyferreiradecarvalho@gmail.com>
2025-02-11 17:23:17 -0300

    finish

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let nomePessoas = [];
+
+
+const adicionarListaAoBody = () => {
+    let elementList = document.querySelector("#listaAmigos");
+    elementList.innerHTML = "";
+ 
+    nomePessoas.forEach(pessoa =>  {
+        let li = document.createElement('li');
+        li.innerHTML = pessoa;
+        elementList.appendChild(li)
+    })
+ }
+
+ 
+const adicionarAmigo = () => {
+   let campoAmigo = document.querySelector('.input-name');
+   const amigo = campoAmigo.value.trim();
+  
+    if(!amigo){
+        alert('Por favor, insira um nome!')
+    } else {
+        nomePessoas.push(amigo)
+        adicionarListaAoBody()
+
+    }
+
+    campoAmigo.value = "";
+}
+
+const gerarNumeroAleatorio = () =>  Math.floor(Math.random() * nomePessoas.length);
+
+function sortearAmigo() {
+    let sorteado = document.querySelector("#resultado");
+    sorteado.innerHTML = '';
+    if(nomePessoas.length === 0){
+       alert('não há amigos disponiveis')
+    } else {
+        const indice  =  gerarNumeroAleatorio()
+        sorteado.innerHTML = `Amigo sorteado ${nomePessoas[indice]}!!!`
+    }
+}
+
+ 
+
